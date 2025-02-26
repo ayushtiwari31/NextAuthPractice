ee01400a42a371c9a2f202cb717ace1a1d1e62ce
Ayush Tiwar <ayush.tiwari0031@gmail.com>
2025-02-26 13:30:10 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 
-import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
-import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css';
 import  { Toaster } from 'react-hot-toast';
 
